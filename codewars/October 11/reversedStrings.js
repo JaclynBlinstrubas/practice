b4e06a8d6c4246f14251e1053716e673ae4937ac
JBlin1819 <jaclyn.blinstrubas@gmail.com>
2021-10-11 14:46:27 -0400

    4 Codewars Kata, Oct 11

---
+++ codewars/October 11/reversedStrings.js
@@ -0,0 +1,6 @@
+// Complete the solution so that it reverses the string passed into it.
+
+function solution(str) {
+  // your code here
+  return str.split('').reverse().join('')
+}
---
+++ codewars/October 11/stringToNumber.js
@@ -0,0 +1,6 @@
+// Convert a string to a number
+
+var stringToNumber = function (str) {
+  var a = parseInt(str)
+  return a
+}
